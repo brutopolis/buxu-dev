06e1e314ae0cbb5694a8723a96123484c33a08c3
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-01 02:34:51 -0300

    minor changes

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 cd buxu
 cp -rf ../bruter .
 rm -rf bruter/.git .buxu
-./build.sh --install
-bpm install-from http://127.0.0.1:3000/packages io std math bit byte ffi string
+./build.sh --install --debug
+bpm install-from http://127.0.0.1:3000/packages io math std os string
 ./build.sh --debug-file example/hello_world.br
