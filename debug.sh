831277440ff6073330f2a301e9d043821f564641
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-10 06:56:59 -0300

    updated to use bupm instead of bpm

--- debug.sh
+++ debug.sh
@@ -2,5 +2,5 @@ cd buxu
 cp -rf ../bruter .
 rm -rf bruter/.git .buxu
 ./build.sh --install --debug
-bpm install-from http://127.0.0.1:3000/packages io math std os string dycc bit byte
+bupm install-from http://127.0.0.1:3000/packages io math std os string dycc bit byte
 ./build.sh --debug-file example/loops.br
