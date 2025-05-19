9c93a5b7daa9aa54fc786d57e8eeba7debecbd3d
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-19 11:14:40 -0300

    minor fix to support br

--- debug.sh
+++ debug.sh
@@ -1,5 +1,6 @@
 cd buxu
 cp -rf ../bruter .
+cp -rf ../br .
 rm -rf bruter/.git .buxu
 ./build.sh --install --debug
 bupm install-from localhost:3000/packages io math std os string dycc bit byte condition
