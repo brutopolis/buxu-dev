e9794dbe7800740d5f08a475849f307dc54f4e09
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-20 13:57:34 -0300

    some minor fixes

--- debug.sh
+++ debug.sh
@@ -3,5 +3,5 @@ cp -rf ../bruter .
 cp -rf ../br .
 rm -rf bruter/.git .buxu
 ./build.sh --install --debug
-bupm install-from localhost:3000/packages io math std os string dycc bit byte condition
-./build.sh --debug-file ../br/example/loops.br
+bupm install-from localhost:3000/packages io math std os string dycc bit byte condition list
+./build.sh --debug-file ../br/example/list.br
