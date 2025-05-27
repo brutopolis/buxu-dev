f7d7fceeb65c88090438526f2078cc02de7b475e
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-27 14:09:37 -0300

    updated to not use buxu experimental branch anymore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 rm -rf buxu bruter packages
 git clone https://github.com/brutopolis/bruter -b experimental
-git clone https://github.com/brutopolis/buxu -b experimental
+git clone https://github.com/brutopolis/buxu # bruter unix utils
 git clone https://github.com/brutopolis/br # bruter representation
-git clone https://github.com/brutopolis/packages
+git clone https://github.com/brutopolis/packages # buxu packages
