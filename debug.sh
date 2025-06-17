eb1156c19936aff1689e00a2130c5f05b8b41b98
jardimdanificado <jardeltorreslima01@gmail.com>
2025-06-17 17:34:32 -0300

    minor change

--- debug.sh
+++ debug.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-sudo rm -rf /usr/bin/buxu /usr/bin/bupm /usr/bin/bucc /usr/include/bruter-representation.h /usr/include/bruter.h
-sudo cp ./bruter-representation/bruter-representation.h /usr/include/bruter-representation.h
-sudo cp ./bruter/bruter.h /usr/include/bruter.h
-sudo cp ./buxu/bupm /usr/bin/bupm
-sudo cp ./buxu/bucc /usr/bin/bucc
-gcc -o buxu.bin ./buxu/buxu.c -ldl -g
-sudo mv buxu.bin /usr/bin/buxu
+source install.sh
 
 
 bupm install-from localhost:3000/packages io math std os string dycc bit byte condition list alloc type #bsr
