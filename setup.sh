d6081b5617787dd4ad5579ed99713c75750d03a9
jardimdanificado <jardeltorreslima01@gmail.com>
2025-06-28 13:20:29 -0300

    removed some unused stuff

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@ git clone https://github.com/brutopolis/bruter -b experimental
 git clone https://github.com/brutopolis/buxu -b experimental # bruter unix utils
 git clone https://github.com/brutopolis/bruter-representation -b experimental # bruter representation
 git clone https://github.com/brutopolis/packages -b experimental # buxu packages
-git clone https://github.com/brutopolis/bruter_cpp # bruter C++ wrapper, not working yet, just a prototype
-git clone https://github.com/brutopolis/cruder # cruder, the old-style bruter
