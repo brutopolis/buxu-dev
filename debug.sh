e3999001355bbf95886858aa498186013d737607
jardimdanificado <jardeltorreslima01@gmail.com>
2025-07-03 12:03:36 -0300

    we now have to specify the language package on buxu call

--- debug.sh
+++ debug.sh
@@ -4,9 +4,9 @@ CC="gcc -O3 -g"
 source install.sh
 
 
-CC="$CC" bupm install-from localhost:3000/packages io math std os string dycc bit byte condition list alloc type #bsr
+CC="$CC" bupm install-from localhost:3000/packages io math std os string dycc bit byte condition list alloc type bruterlang #bsr
 
-valgrind --tool=massif --stacks=yes --detailed-freq=1 --verbose  buxu $1
+valgrind --tool=massif --stacks=yes --detailed-freq=1 --verbose  buxu -lbruterlang $1
 ms_print massif.out.* > ./massif-out.txt
 rm massif.out.*
 
@@ -15,4 +15,4 @@ valgrind \
     --show-leak-kinds=all \
     --track-origins=yes \
     --log-file=./valgrind-out.txt \
-    --verbose buxu $1
+    --verbose buxu -lbruterlang $1
