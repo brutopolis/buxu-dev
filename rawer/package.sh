56cf1142e25e7102f51896117f79e836cabde88c
jardimdanificado <jardeltorreslima01@gmail.com>
2025-07-17 21:58:13 -0300

    minor changes

--- rawer/package.sh
+++
@@ -1,8 +0,0 @@
-name rawer
-version 0.0.4
-description "rawer"
-# files 
-setup()
-{
-    gcc -O3 -Wl,-rpath=/lib -o rawer main.c std.c -g
-}
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/brutopolis/bruter -b experimental
 git clone https://github.com/brutopolis/buxu -b experimental # bruter unix utils
 git clone https://github.com/brutopolis/bruter-representation -b experimental # bruter representation
 git clone https://github.com/brutopolis/packages -b experimental # buxu packages
+git clone https://github.com/brutopolis/rawer # rawer
