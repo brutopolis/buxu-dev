81ed1160cd36a5072ea9ba9af123571db1b580f2
jardimdanificado <jardeltorreslima01@gmail.com>
2025-07-26 08:59:04 -0300

    br and buxu not mantained anymore

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@ if [ -z "$CC" ]; then
     CC="gcc -O3"
 fi
 
-sudo rm -rf /usr/bin/buxu /usr/bin/bpm /usr/include/bruter-representation.h /usr/include/bruter.h
-sudo cp ./bruter-representation/bruter-representation.h /usr/include/bruter-representation.h
+sudo rm -rf /usr/bin/bpm /usr/include/bruter.h
 sudo cp ./bruter/bruter.h /usr/include/bruter.h
-sudo cp ./buxu/bpm /usr/bin/bpm
-$CC -o buxu.bin ./buxu/buxu.c -ldl
-sudo mv buxu.bin /usr/bin/buxu
+sudo cp ./bruter/bpm /usr/bin/bpm
--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 rm -rf buxu bruter packages bruter-representation
 git clone https://github.com/brutopolis/bruter -b experimental
-git clone https://github.com/brutopolis/buxu -b experimental # bruter unix utils
-git clone https://github.com/brutopolis/bruter-representation -b experimental # bruter representation
 git clone https://github.com/brutopolis/packages -b experimental # buxu packages
 git clone https://github.com/brutopolis/rawer # rawer
