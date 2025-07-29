1f0c0f3972d7d39e0ee2399b649c5eab0a5baef7
jardimdanificado <jardeltorreslima01@gmail.com>
2025-07-28 21:41:08 -0300

    install updated

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ if [ -z "$CC" ]; then
     CC="gcc -O3"
 fi
 
-sudo rm -rf /usr/bin/bpm /usr/include/bruter.h
+sudo rm -rf /usr/bin/bpm /usr/include/bruter.h /usr/include/rawer.h
 sudo cp ./bruter/bruter.h /usr/include/bruter.h
+sudo cp ./rawer/rawer.h /usr/include/rawer.h
 sudo cp ./bruter/bpm /usr/bin/bpm
