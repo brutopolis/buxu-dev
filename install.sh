5dfb2aafa22d6d35bf145e8b26da66bf18078c2a
jardimdanificado <jardeltorreslima01@gmail.com>
2025-08-14 14:53:27 -0300

    removed bpm from install script

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ if [ -z "$CC" ]; then
     CC="gcc -O3"
 fi
 
-sudo rm -rf /usr/bin/bpm /usr/include/bruter.h /usr/include/feraw.h
+sudo rm -rf /usr/include/bruter.h /usr/include/feraw.h
 sudo cp ./bruter/bruter.h /usr/include/bruter.h
-sudo cp ./bruter/bpm /usr/bin/bpm
