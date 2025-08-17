2dddbd2c2145fa4921ed68ce05361a57cd689e07
jardimdanificado <jardeltorreslima01@gmail.com>
2025-08-17 03:09:53 -0300

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-rm -rf buxu bruter packages bruter-representation
+rm -rf buxu bruter
 git clone https://github.com/brutopolis/bruter -b experimental
-git clone https://github.com/brutopolis/packages -b experimental # buxu packages
 git clone https://github.com/brutopolis/feraw # feraw
